0d46dbd60243c7ac954b2bf27f644efc6b56bd9f
Jorge Acosta <61467132+JLAcostaEC@users.noreply.github.com>
2025-02-18 15:33:18 -0400

    fix: prevent test to be compiled at build time and improve linter

---
+++ .changeset/angry-symbols-shine.md
@@ -0,0 +1,5 @@
+---
+'@jlacostaec/svgtosvelte': patch
+---
+
+fix: prevent test to be compiled at build time and improve linter
--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 
@@ -8,6 +9,8 @@ const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default tseslint.config(
   includeIgnoreFile(gitignorePath),
+  ...tseslint.configs.recommended,
+  prettier,
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
